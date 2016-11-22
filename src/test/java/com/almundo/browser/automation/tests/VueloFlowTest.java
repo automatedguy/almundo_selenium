6ee746eb57c05003355f0678f00ff070f9afc8ac
gabi <gabriel.cespedes@almundo.com>
2016-11-22 18:13:34 -0300

    AQ-35: added some comments - related to Select approach

--- src/test/java/com/almundo/browser/automation/tests/VueloFlowTest.java
+++ src/test/java/com/almundo/browser/automation/tests/VueloFlowTest.java
@@ -33,15 +33,13 @@ public class VueloFlowTest extends TestBaseSetup {
 
         numPassengers = vueloFlow.selectPassenger(driver, numAdults, numChilds);
 
-
-
-        //Select claseVueloDdl = new Select(driver.findElement(VueloFlowMap.CLASE_DDL.getBy()));
-        driver.findElement(VueloFlowMap.CLASE_DDL.getBy()).sendKeys(claseVuelo);
-
+        // commented the Select approach as it is not working for firefox
+        // Select claseVueloDdl = new Select(driver.findElement(VueloFlowMap.CLASE_DDL.getBy()));
         // vueloFlow.clickOn(driver, VueloFlowMap.CLASE_DDL.getBy());
-
         // claseVueloDdl.selectByVisibleText(claseVuelo);
 
+        driver.findElement(VueloFlowMap.CLASE_DDL.getBy()).sendKeys(claseVuelo);
+        
         vueloFlow.clickOn(driver, BaseFlowMap.BUSCAR_BTN.getBy());
 
         if (vueloFlow.nothingFound(driver)) {
