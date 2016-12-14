26cf7e9779f9e5f634ed0f3d319809d2a45f2b70
gabi <gabriel.cespedes@almundo.com>
2016-12-14 11:35:01 -0300

    AQ-58: added VueloHotelResultsPage

---
+++ src/test/java/com/almundo/browser/automation/pages/ResultsPage/VueloHotelResultsPage.java
@@ -0,0 +1,21 @@
+package com.almundo.browser.automation.pages.ResultsPage;
+
+import com.almundo.browser.automation.base.TestBaseSetup;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by gabrielcespedes on 14/12/16.
+ */
+public class VueloHotelResultsPage extends TestBaseSetup {
+
+    public VueloHotelResultsPage(WebDriver driver) {
+        super.driver = driver;
+    }
+
+    //############################################### Locators ##############################################
+
+
+
+    //############################################### Actions ##############################################
+
+}
