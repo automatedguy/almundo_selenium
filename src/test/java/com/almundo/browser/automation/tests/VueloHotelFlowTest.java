7cd71fc42312ed676b0ee012c6f117eec4a39aee
gabi <gabriel.cespedes@almundo.com>
2016-12-16 16:43:30 -0300

    AQ-59: refactored VueloHotelFlowTest

--- src/test/java/com/almundo/browser/automation/tests/VueloHotelFlowTest.java
+++ src/test/java/com/almundo/browser/automation/tests/VueloHotelFlowTest.java
@@ -1,13 +1,14 @@
 package com.almundo.browser.automation.tests;
 
 import com.almundo.browser.automation.base.TestBaseSetup;
+import com.almundo.browser.automation.pages.BasePage.BasePage;
 import com.almundo.browser.automation.pages.CheckOutPage.CheckOutPage;
 import com.almundo.browser.automation.pages.ResultsPage.VueloHotelDetailPage;
 import com.almundo.browser.automation.pages.ResultsPage.VueloHotelResultsPage;
-import com.almundo.browser.automation.utils.JsonRead;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import com.almundo.browser.automation.utils.PageUtils;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 /**
  * Created by gabrielcespedes on 04/11/16.
@@ -19,101 +20,61 @@ public class VueloHotelFlowTest extends TestBaseSetup {
     private VueloHotelDetailPage vueloHotelDetailPage = null;
     private CheckOutPage checkOutPage = null;
 
-    private JSONObject vueloHotelList = null;
-    private JSONObject vueloHotel = null;
-
-    private JSONObject billingData = null;
-    private JSONObject contactData = null;
-    private JSONObject creditCardData = null;
-    private JSONObject passengerData = null;
-
-    private JSONArray passengerJsonList = new JSONArray();
-
-    private String originAuto;
-    private String originFull;
-    private String destinationAuto;
-    private String destinationFull;
-    private int startDate;
-    private int endDate;
-    private int adults;
-    private int childs;
-    private int rooms;
-
     @BeforeClass
-    private void getVuelosListDataObject() {
-        vueloHotelList = JsonRead.getJsonDataObject(jsonDataObject, "vueloHotel", countryPar.toLowerCase() + "_data.json");
+    private void initDataTripList() {
+        basePage = new BasePage(driver);
+        basePage.vueloHotelDataTrip().getVueloHotelDataTripList();
+
+        checkOutPage = initCheckOutPage();
+        checkOutPage.passengerSection().getPassengersList();
+        checkOutPage.creditCardSection().getCreditCardList();
+        checkOutPage.billingSection().getBillingList();
+        checkOutPage.contactSection().getContactList();
     }
-
-    private void getVueloHotelDataObject(String combination) {
-        vueloHotel = JsonRead.getJsonDataObject(vueloHotelList, combination, countryPar.toLowerCase() + "_data.json");
-
-        originAuto = vueloHotel.get("originAuto").toString();
-        originFull = vueloHotel.get("originFull").toString();
-
-        destinationAuto = vueloHotel.get("destinationAuto").toString();
-        destinationFull = vueloHotel.get("destinationFull").toString();
-
-        startDate = Integer.parseInt(vueloHotel.get("startDate").toString());
-        endDate = Integer.parseInt(vueloHotel.get("endDate").toString());
-
-        adults = Integer.parseInt(vueloHotel.get("adults").toString());
-        childs = Integer.parseInt(vueloHotel.get("childs").toString());
-
-        rooms = Integer.parseInt(vueloHotel.get("rooms").toString());
-
-    }
-
-//    private void getPassengerDataObject(String dataSet)  {
-//        passengerData = JsonRead.getJsonDataObject(passengersList, dataSet, countryPar.toLowerCase() + "_data.json");
-//        passengerJsonList.add(passengerData);
-//    }
-//
-//    private void getCreditCardDataObject(String dataSet)  {
-//        creditCardData = JsonRead.getJsonDataObject(CheckOutPage.getCreditCardListObject(), dataSet, countryPar.toLowerCase() + "_data.json");
-//    }
-//
-//    private void getBillingDataObject(String dataSet)  {
-//        billingData = JsonRead.getJsonDataObject(CheckOutPage.getBillingListObject(), dataSet, countryPar.toLowerCase() + "_data.json");
-//    }
-//
-//    private void getContactDataObject(String dataSet)  {
-//        contactData = JsonRead.getJsonDataObject(CheckOutPage.getContactsListObject(), dataSet, countryPar.toLowerCase() + "_data.json");
-//    }
 
     /////////////////////////////////// TEST CASES ///////////////////////////////////
-//
-//    @Test
-//    public void vueloHotelReservationFirstOptionFlow() throws InterruptedException {
-//        getVueloHotelDataObject("miami_10days_2adults_2childs_1room");
-//        getBillingDataObject("local_Billing");
-//        getContactDataObject("contact_cell_phone");
-//        getCreditCardDataObject("amex");
-//
-//        getPassengersListObject();
-//        getPassengerDataObject("adult_male_passport_native");
-//        getPassengerDataObject("adult_male_passport_native");
-//        getPassengerDataObject("child_male_passport_native");
-//        getPassengerDataObject("child_male_passport_native");
-//
-//        PageUtils.waitElementForVisibility(driver, basePage.vueloHotelIcon, 10, "Vuelo+Hotel icon");
-//        basePage.vueloHotelIcon.click();
-//
-//        basePage.vueloHotelDataTrip().setOrigin(originAuto, originFull);
-//        basePage.vueloHotelDataTrip().setDestination(destinationAuto, destinationFull);
-//
-//        basePage.vueloHotelDataTrip().selectDateFromCalendar(basePage.vueloHotelDataTrip().departureCalendar, startDate);
-//        basePage.vueloHotelDataTrip().selectDateFromCalendar(basePage.vueloHotelDataTrip().arrivalCalendar, endDate);
-//
-//        numPassengers = basePage.vueloHotelDataTrip().selectPassenger(adults, childs, rooms);
-//
-//        vueloHotelResultsPage = basePage.vueloHotelDataTrip().clickBuscarBtn();
-//
-//        Assert.assertTrue(vueloHotelResultsPage.vacancy());
-//
-//        vueloHotelResultsPage.clickElegirBtn(0);
-//        vueloHotelDetailPage = vueloHotelResultsPage.clickContinuarBtn();
-//        vueloHotelDetailPage.clickVerHabitacionBtn();
-//        checkOutPage = vueloHotelDetailPage.clickComprarBtn(0);
-//        checkOutPage.populateCheckOutPage(billingData, contactData, creditCardData, passengerJsonList, numPassengers);
-//    }
+
+    @Test
+    public void vueloHotelReservationFirstOptionFlow() throws InterruptedException {
+        PageUtils.waitElementForVisibility(driver, basePage.vueloHotelIcon, 10, "Vuelo+Hotel icon");
+        basePage.vueloHotelIcon.click();
+
+        basePage.vueloHotelDataTrip().getVueloHotelDataTripItinerary("miami_10days_2adults_2childs_1room");
+
+        basePage.vueloHotelDataTrip().setOrigin(basePage.vueloHotelDataTrip().originAuto, basePage.vueloHotelDataTrip().originFull);
+        basePage.vueloHotelDataTrip().setDestination(basePage.vueloHotelDataTrip().destinationAuto, basePage.vueloHotelDataTrip().destinationFull);
+
+        basePage.vueloHotelDataTrip().selectDateFromCalendar(basePage.vueloHotelDataTrip().departureCalendar, basePage.vueloHotelDataTrip().startDate);
+        basePage.vueloHotelDataTrip().selectDateFromCalendar(basePage.vueloHotelDataTrip().arrivalCalendar, basePage.vueloHotelDataTrip().endDate);
+
+        numPassengers = basePage.vueloHotelDataTrip().selectPassenger(basePage.vueloHotelDataTrip().adults,
+                                                                      basePage.vueloHotelDataTrip().childs,
+                                                                      basePage.vueloHotelDataTrip().rooms);
+
+        vueloHotelResultsPage = basePage.vueloHotelDataTrip().clickBuscarBtn();
+
+        Assert.assertTrue(vueloHotelResultsPage.vacancy());
+
+        vueloHotelResultsPage.clickElegirBtn(0);
+        vueloHotelDetailPage = vueloHotelResultsPage.clickContinuarBtn();
+        vueloHotelDetailPage.clickVerHabitacionBtn();
+
+        checkOutPage = vueloHotelDetailPage.clickComprarBtn(0);
+
+        checkOutPage.passengerSection().getPassengerData("adult_male_passport_native");
+        checkOutPage.passengerSection().getPassengerData("adult_male_passport_native");
+        checkOutPage.passengerSection().getPassengerData("child_male_passport_native");
+        checkOutPage.passengerSection().getPassengerData("child_male_passport_native");
+
+        checkOutPage.creditCardSection().getCreditCardData("amex");
+        checkOutPage.billingSection().getBillingData("local_Billing");
+        checkOutPage.contactSection().getContactData("contact_cell_phone");
+
+
+        checkOutPage.populateCheckOutPage(numPassengers,
+                                          checkOutPage.passengerSection().passengerJsonList,
+                                          checkOutPage.creditCardSection().creditCardData,
+                                          checkOutPage.billingSection().billingData,
+                                          checkOutPage.contactSection().contactData);
+    }
 }
