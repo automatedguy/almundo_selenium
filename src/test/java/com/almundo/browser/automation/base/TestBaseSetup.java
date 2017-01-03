a23906e44df81dd8a863e3a8cc1ce193e2e7b107
gabi <gabriel.cespedes@almundo.com>
2017-01-03 12:04:15 -0300

    Updated passengers paramaters for COLOMBIA Vuelos

--- src/test/java/com/almundo/browser/automation/base/TestBaseSetup.java
+++ src/test/java/com/almundo/browser/automation/base/TestBaseSetup.java
@@ -60,7 +60,7 @@ public class TestBaseSetup {
                                         //@Optional("OS X 10.11") String osType,
                                         @Optional("firefox") String browserType,
                                         @Optional("latest") String browserTypeVersion,
-                                        @Optional("ARGENTINA") String country) {
+                                        @Optional("COLOMBIA") String country) {
 
         this.baseURL = env_url;
         this.os = osType;
