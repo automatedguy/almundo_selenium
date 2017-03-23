244d63d14488e4864e4dfdabe1d77f3e04324375
Gabriel Cespedes <gabriel.cespedes@almundo.com>
2017-03-23 16:08:50 -0300

    Added ComprarBtn For CheckOutV3

--- src/test/java/com/almundo/browser/automation/pages/ResultsPage/TripsDetailPage.java
+++ src/test/java/com/almundo/browser/automation/pages/ResultsPage/TripsDetailPage.java
@@ -2,6 +2,7 @@ package com.almundo.browser.automation.pages.ResultsPage;
 
 import com.almundo.browser.automation.base.TestBaseSetup;
 import com.almundo.browser.automation.pages.CheckOutPage.CheckOutPage;
+import com.almundo.browser.automation.pages.CheckOutPageV3.CheckOutPageV3;
 import com.almundo.browser.automation.utils.PageUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -38,6 +39,15 @@ public class TripsDetailPage extends TestBaseSetup {
         logger.info("Clicking on Comprar button index: [" + index + "]");
         comprarBtn.get(index).click();
         return initCheckOutPage();
+    }
+
+    public CheckOutPageV3 clickComprarBtnV3(int index) {
+        String cssSelectorName = ".select-room-button.button.button--md.button--secondary";
+        PageUtils.waitListContainResults(driver, cssSelectorName, 0);
+        List<WebElement> comprarBtn = driver.findElements(By.cssSelector(cssSelectorName));
+        logger.info("Clicking on Comprar button index: [" + index + "]");
+        comprarBtn.get(index).click();
+        return initCheckOutPageV3();
     }
 
 }
