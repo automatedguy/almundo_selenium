2dc2c501dd796af62688627a21420c9b6e2839a8
Gabriel Cespedes <gabriel.cespedes@almundo.com>
2017-03-27 14:21:59 -0300

    Fixed Retail Colombia

--- src/test/java/com/almundo/browser/automation/base/TestBaseSetup.java
+++ src/test/java/com/almundo/browser/automation/base/TestBaseSetup.java
@@ -58,7 +58,7 @@ public class TestBaseSetup {
 
     @Parameters({"env", "osType", "browserType", "browserTypeVersion", "country", "landing", "cart_id", "cart_id_icbc", "retries_Max_Count"})
     @BeforeSuite
-    public void initializeTestBaseSetup(@Optional(PROD_URL) String env_url,
+    public void initializeTestBaseSetup(@Optional("https://sucursales.almundo.com.co") String env_url,
                                         @Optional() String osType,
 //                                        @Optional("OS X 10.11") String osType,
 //                                        @Optional("Windows 10") String osType,
