9649f3552a2a772f4e7547d269c06f4956f586c3
Gabriel Cespedes <gabriel.cespedes@almundo.com>
2017-04-03 18:11:34 -0300

    Added loggers for Trips flows

--- src/test/java/com/almundo/browser/automation/pages/ResultsPage/TripsResultsPage.java
+++ src/test/java/com/almundo/browser/automation/pages/ResultsPage/TripsResultsPage.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.support.FindBy;
 import java.util.List;
 
 import static com.almundo.browser.automation.utils.Constants.Messages.*;
+import static com.almundo.browser.automation.utils.PageUtils.formatInfo;
 
 /**
  * Created by gabrielcespedes on 14/12/16.
@@ -25,10 +26,58 @@ public class TripsResultsPage extends TestBaseSetup {
     @FindBy(id = "continue-v2")
     private WebElement continuarBtn;
 
+    @FindBy(css = ".details>hotel-summary")
+    private WebElement detailsHotelSummary;
+
+
     //############################################### Actions ##############################################
+
+    public String getHotelName(){
+        return formatInfo(detailsHotelSummary.getText());
+    }
+
+    public List<WebElement> getHotelAmenities(){
+        List<WebElement> hotelAmenitiesList = driver.findElements(By.cssSelector(".hotel.active > div > div.details > hotel-summary > div.amenities > span > div"));
+        logger.info("Checking Hotel Amenities...");
+        return hotelAmenitiesList;
+    }
+
+    public String getHotelStars(){
+        List<WebElement> hotelStarsList = driver.findElements(By.cssSelector(".hotel.active > div > div.details > hotel-summary > star-rating .icon-star.star--lg"));
+        return String.valueOf(hotelStarsList.size());
+    }
+
+    public void displayHotelInfo() {
+        int hotelAmenityIndex = 1;
+        logger.info("Hotel name: " + getHotelName());
+        logger.info("Hotel category: " + "[" + getHotelStars() + " stars]");
+        for (WebElement amenity : getHotelAmenities()) {
+            logger.info("Hotel amenity " + hotelAmenityIndex++ + ": [" + amenity.getAttribute("aria-label").toString() + "]");
+        }
+    }
+
+    public void displayDepartureFlightInfo(WebElement departureFlightInfo) {
+        logger.info("Departure Flight Info: " + "[" +  formatInfo(departureFlightInfo.getText())+ "]");
+    }
+
+    public void displayReturnFlightInfo(WebElement returnFlightInfo) {
+        logger.info("Return Flight Info: " + "[" + formatInfo(returnFlightInfo.getText()) + "]");
+    }
+
+    public void displayFlightInfo(){
+        List<WebElement> flightSegmentsList = driver.findElements(By.cssSelector(".content>flight-segment"));
+        displayDepartureFlightInfo(flightSegmentsList.get(0));
+        displayReturnFlightInfo(flightSegmentsList.get(1));
+    }
+
+    public void displayTripInfo(){
+        displayHotelInfo();
+        displayFlightInfo();
+    }
 
     public TripsDetailPage clickContinuarBtn() {
         PageUtils.waitElementForVisibility(driver,continuarBtn,30, "Continuar Button");
+        displayTripInfo();
         logger.info("Clicking on Continuar button");
         continuarBtn.click();
         PageUtils.waitImplicitly(2000);
