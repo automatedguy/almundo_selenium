091c10c0862553070db710d4ec121abd7b1d8213
leandroefron <leandro.efron@almundo.com>
2017-04-03 16:00:45 -0300

    Implement getMaxCount for retries

--- src/test/java/com/almundo/browser/automation/utils/RetryAnalyzer.java
+++ src/test/java/com/almundo/browser/automation/utils/RetryAnalyzer.java
@@ -7,9 +7,9 @@ import org.testng.Reporter;
 import static com.almundo.browser.automation.base.TestBaseSetup.retriesMaxCount;
 
 public class RetryAnalyzer implements IRetryAnalyzer {
-    private int count = 3;
+    private int maxCount;
 
-    public int getMaxount() {
+    public int getMaxCount() {
         String env_path = System.getenv("PATH");
         int maxCount = 0;
 
@@ -22,13 +22,13 @@ public class RetryAnalyzer implements IRetryAnalyzer {
     @Override
     public boolean retry(ITestResult result) {
         if (!result.isSuccess()) {
-            if (count < retriesMaxCount) {
-                count++;
+            if (getMaxCount() < retriesMaxCount) {
+                maxCount++;
                 result.setStatus(ITestResult.SUCCESS_PERCENTAGE_FAILURE);
                 String message = Thread.currentThread().getName() +
                         "Error in " + result.getName() +
                         " with status " + result.getStatus() +
-                        " Retrying " + count + " times";
+                        " Retrying " + maxCount + " times";
                 System.out.println(message);
                 Reporter.log(message);
 
