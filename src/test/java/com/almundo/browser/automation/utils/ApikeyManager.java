b029ff26944a0fe1d7cdb9c4ef1820340f2c3f38
gabi <automationthings@gmail.com>
2017-04-20 22:21:22 -0300

    Small fixes running good.

--- src/test/java/com/almundo/browser/automation/utils/ApikeyManager.java
+++ src/test/java/com/almundo/browser/automation/utils/ApikeyManager.java
@@ -2,9 +2,9 @@ package com.almundo.browser.automation.utils;
 
 import com.jayway.jsonpath.JsonPath;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -20,7 +20,7 @@ import java.util.Map;
  */
 public class ApikeyManager {
 
-    private HttpClient httpClient = new DefaultHttpClient();
+    CloseableHttpClient httpClient = HttpClientBuilder.create().build();
     private String apiKeyEndPoint = "http://abs.almundo.it:8080/abs/security/apikeys";
     public String apiKeyHeader;
 
@@ -31,17 +31,13 @@ public class ApikeyManager {
 
     private HttpResponse getHttpResponse() {
         HttpResponse httpResponse = null;
-        try {
-            httpResponse = httpClient.execute(createHttpRequest());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return httpResponse;
+        try { httpResponse = httpClient.execute(createHttpRequest());
+        } catch (IOException e) { e.printStackTrace();
+        } return httpResponse;
     }
 
     private JSONArray parseHttpResponse() throws IOException, ParseException {
-        Object json;
-        JSONParser parser = new JSONParser();
+        Object json; JSONParser parser = new JSONParser();
         json = parser.parse(EntityUtils.toString(getHttpResponse().getEntity(), "UTF-8"));
         JSONArray jsonArray = (JSONArray) json;
         return jsonArray;
@@ -49,38 +45,25 @@ public class ApikeyManager {
 
     private JSONArray getApikeysArray(){
         JSONArray jsonArray = null;
-        try {
-            jsonArray = parseHttpResponse();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return jsonArray;
+        try { jsonArray = parseHttpResponse();
+        } catch (IOException e) { e.printStackTrace();
+        } catch (ParseException e) { e.printStackTrace();
+        } return jsonArray;
     }
 
     private String getApiKey(JSONArray jsonArray){
-        JSONObject jsonApikeySection;
-        boolean apikeyFound = false;
-        int index = -1;
-        do{
-            index++;
-            jsonApikeySection = new JSONObject((Map) jsonArray.get(index));
-            if(JsonPath.read(jsonApikeySection, "$.name").equals("ccr-corporate")){
+        JSONObject jsonApikeySection; boolean apikeyFound = false; int index = -1;
+        do{ index++; jsonApikeySection = new JSONObject((Map) jsonArray.get(index));
+            if(JsonPath.read(jsonApikeySection, "$.name").equals("almundo-web")){
                 JsonPath.read(jsonApikeySection, "$.value");
                 apikeyFound = true;}
         }while(!apikeyFound);
         return JsonPath.read(jsonApikeySection, "$.value");
     }
 
-    private  void closeConnection(){
-        httpClient.getConnectionManager().shutdown();
-    }
-
     @Test
     public void checkApiKey(){
         apiKeyHeader = getApiKey(getApikeysArray());
         System.out.println("Your APIKEY: " + apiKeyHeader);
-        closeConnection();
     }
 }
