800b1eb8068b8514e908bd70de15f4307cacb4aa
Gabriel Cespedes <gabriel.cespedes@almundo.com>
2017-07-07 11:56:39 -0300

    Implemented Regressions For Flights

--- src/test/java/com/almundo/browser/automation/tests/Checkout/Flights.java
+++ src/test/java/com/almundo/browser/automation/tests/Checkout/Flights.java
@@ -42,13 +42,13 @@ public class Flights extends TestBaseSetup {
     }
 
     private void getPassengersData(){
+        logger.info("Getting Passenger Data.");
         dataManagement.getPassengerData("adult_male_native");
         dataManagement.getPassengerData("adult_female_native");
         dataManagement.getPassengerData("child_male_native");
-
     }
-
-    /************************ Test Area ************************/
+    
+    /************************ Grid Test Area ************************/
 
     @Test
     public void gridWithTodoPago() {
@@ -69,9 +69,317 @@ public class Flights extends TestBaseSetup {
     }
 
     @Test
+    public void gridExplicitWithTodoPago() {
+        logTestTitle("Flights – Grid (explicit) With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridExplicitWithTodoPagoExplicit() {
+        logTestTitle("Flights – Grid (explicit) With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0&stp=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithoutTodoPagoExplicit() {
+        logTestTitle("Flights – Grid Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&stp=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithDebitExplicitWithTodoPago() {
+        logTestTitle("Flights – Grid With Debit (explicit)  With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridExplicitWithDebitExplicitWithTodoPago() {
+        logTestTitle("Flights – Grid (explicit)  With Debit (explicit)  With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0&svd=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridExplicitWithDebitExplicitWithTodoPagoExplicit() {
+        logTestTitle("Flights – Grid (explicit)  With Debit (explicit)  With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0&svd=1&stp=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithDebitExplicitWithoutTodoPago() {
+        logTestTitle("Flights – Grid With Debit (explicit) Without Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=1&stp=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void  gridWithoutDebitExplicitWithTodoPago() {
+        logTestTitle("Flights – Grid Without Debit (explicit) Without Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithoutDebitExplicitWithoutTodoPagoExplicit() {
+        logTestTitle("Flights – Grid Without Debit (explicit) Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=0&stp=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    /************************ Combo Test Area ************************/
+
+    @Test
     public void comboWithTodoPago() {
         logTestTitle("Flights – Combo With Todo Pago " + countryPar );
         checkOutPageV3 = openCart(cartId, "&sc=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithTodoPagoExplicit() {
+        logTestTitle("Flights – Combo With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&stp=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutTodoPago() {
+        logTestTitle("Flights – Combo Without Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&stp=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithDebitExplicitWithTodoPago() {
+        logTestTitle("Flights – Combo With Debit (explicit) With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithDebitExplicitWithTodoPagoExplicit() {
+        logTestTitle("Flights – Combo With Debit (explicit) With Todo Pago  (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=1&stp=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithDebitExplicitWithoutTodoPagoExplicit() {
+        logTestTitle("Flights – Combo With Debit (explicit) Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=1&stp=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutDebitExplicitWithTodoPago() {
+        logTestTitle("Flights – Combo Without Debit (explicit) With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=0");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutDebitExplicitWithTodoPagoExplicit() {
+        logTestTitle("Flights – Combo Without Debit (explicit) With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=0&stp=1");
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "FlightsCheckOutPageInternational");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutDebitExplicitWithoutTodoPagoExplicit() {
+        logTestTitle("Flights – Combo Without Debit (explicit) Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=0&stp=0");
 
         getPassengersData();
 
