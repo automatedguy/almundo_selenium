6b3abae5d27d2aa2de844fa8a6f5ffa15b8116aa
Gabriel Cespedes <gabriel.cespedes@almundo.com>
2017-07-10 11:44:08 -0300

    Refactored Checkout Tests (HOTELS)

--- src/test/java/com/almundo/browser/automation/tests/Checkout/Hotels.java
+++ src/test/java/com/almundo/browser/automation/tests/Checkout/Hotels.java
@@ -40,12 +40,12 @@ public class Hotels extends TestBaseSetup {
         dataManagement.getPassengerData("adult_female_native");
     }
 
-    /************************ Test Area ************************/
+    /************************ Grid Test Area ************************/
 
     @Test
     public void gridWithTodoPago() {
         logTestTitle("Hotels – Grid With Todo Pago " + countryPar );
-        checkOutPageV3 = openCart(cartId, "", productURl);
+        checkOutPageV3 = openCart(cartId, "",productURl);
 
         getPassengersData();
 
@@ -61,9 +61,317 @@ public class Hotels extends TestBaseSetup {
     }
 
     @Test
+    public void gridExplicitWithTodoPago() {
+        logTestTitle("Hotels – Grid (explicit) With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridExplicitWithTodoPagoExplicit() {
+        logTestTitle("Hotels – Grid (explicit) With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0&stp=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithoutTodoPagoExplicit() {
+        logTestTitle("Hotels – Grid Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&stp=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithDebitExplicitWithTodoPago() {
+        logTestTitle("Hotels – Grid With Debit (explicit)  With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridExplicitWithDebitExplicitWithTodoPago() {
+        logTestTitle("Hotels – Grid (explicit)  With Debit (explicit)  With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0&svd=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridExplicitWithDebitExplicitWithTodoPagoExplicit() {
+        logTestTitle("Hotels – Grid (explicit)  With Debit (explicit)  With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=0&svd=1&stp=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithDebitExplicitWithoutTodoPago() {
+        logTestTitle("Hotels – Grid With Debit (explicit) Without Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=1&stp=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void  gridWithoutDebitExplicitWithTodoPago() {
+        logTestTitle("Hotels – Grid Without Debit (explicit) Without Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void gridWithoutDebitExplicitWithoutTodoPagoExplicit() {
+        logTestTitle("Hotels – Grid Without Debit (explicit) Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&svd=0&stp=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    /************************ Combo Test Area ************************/
+
+    @Test
     public void comboWithTodoPago() {
         logTestTitle("Hotels – Combo With Todo Pago " + countryPar );
         checkOutPageV3 = openCart(cartId, "&sc=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithTodoPagoExplicit() {
+        logTestTitle("Hotels – Combo With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&stp=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutTodoPago() {
+        logTestTitle("Hotels – Combo Without Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&stp=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithDebitExplicitWithTodoPago() {
+        logTestTitle("Hotels – Combo With Debit (explicit) With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithDebitExplicitWithTodoPagoExplicit() {
+        logTestTitle("Hotels – Combo With Debit (explicit) With Todo Pago  (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=1&stp=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithDebitExplicitWithoutTodoPagoExplicit() {
+        logTestTitle("Hotels – Combo With Debit (explicit) Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=1&stp=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutDebitExplicitWithTodoPago() {
+        logTestTitle("Hotels – Combo Without Debit (explicit) With Todo Pago " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=0", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutDebitExplicitWithTodoPagoExplicit() {
+        logTestTitle("Hotels – Combo Without Debit (explicit) With Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=0&stp=1", productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                "1_visa_visa",
+                dataManagement.getBillingData("local_Billing"),
+                dataManagement.getContactData("contact_cell_phone"),
+                "HotelsCheckOutPageInternationalV3");
+
+        confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(confirmationPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void comboWithoutDebitExplicitWithoutTodoPagoExplicit() {
+        logTestTitle("Hotels – Combo Without Debit (explicit) Without Todo Pago (explicit) " + countryPar );
+        checkOutPageV3 = openCart(cartId, "&sc=1&svd=0&stp=0", productURl);
 
         getPassengersData();
 
