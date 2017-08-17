47a88f3694b25c3824c541acb5062917d7e8400e
Gabriel Cespedes <gabriel.cespedes@almundo.com>
2017-08-17 12:36:17 -0300

    AQ-219 and fixed AMEX for COLOMBIA

--- src/test/java/com/almundo/browser/automation/tests/Flights/RetailFlowTest.java
+++ src/test/java/com/almundo/browser/automation/tests/Flights/RetailFlowTest.java
@@ -127,7 +127,7 @@ public class RetailFlowTest extends TestBaseSetup {
 
     @Test
     public void suc_Int_Booking_Flow_Splitted_2cards() {
-        logTestTitle("Sucursales Flight Flow - International - Splitted 20 days - 2 Adults - Todas - " + countryPar);
+        logTestTitle("Sucursales Flight Flow - International - Splitted 2 Cards - 20 days - 2 Adults - Todas - " + countryPar);
 
         if(!countryPar.equals("MEXICO")) {
             dataManagement.getRoundTripDataTripItinerary("miami_10days_2adults_turista");
@@ -155,6 +155,135 @@ public class RetailFlowTest extends TestBaseSetup {
 
             checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
                     "pago_dividido$1_visa_visa$1_master_master$",
+                    dataManagement.getBillingData("local_Billing_sucursales"),
+                    dataManagement.getContactData("contact_phone"),
+                    "FlightsCheckOutPageInternationalSucursal");
+
+            confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+            Assert.assertTrue(confirmationPageV3.confirmationOk());
+        }
+        else{
+            logger.warn("We are not running this for MEXICO!");
+        }
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void suc_Int_Booking_Flow_Splitted_DepositCredit() {
+        logTestTitle("Sucursales Flight Flow - International - Splitted Deposit / Credit - 20 days - 2 Adults - Todas - " + countryPar);
+
+        if(!countryPar.equals("MEXICO")) {
+            dataManagement.getRoundTripDataTripItinerary("miami_10days_2adults_turista");
+
+            flightsDataTrip = basePage.flightsDataTrip();
+            flightsDataTrip.setOrigin(dataManagement.originAuto, dataManagement.originFull);
+            flightsDataTrip.setDestination(dataManagement.destinationAuto, dataManagement.destinationFull);
+            flightsDataTrip.selectDateFromCalendar(flightsDataTrip.departureFlightsCalendar, dataManagement.startDate);
+            flightsDataTrip.selectDateFromCalendar(flightsDataTrip.arrivalFlightsCalendar, dataManagement.endDate);
+            flightsDataTrip.selectPassenger(dataManagement.adults, dataManagement.childs);
+            flightsDataTrip.selectChildAgeRange(dataManagement.childAgeRange, dataManagement.childs);
+            flightsDataTrip.selectClass(dataManagement.flightClass);
+
+            flightsResultsPage = flightsDataTrip.clickBuscarBtn();
+
+            Assert.assertTrue(flightsResultsPage.vacancy());
+
+            flightsResultsPage.clickTicketIdaRdb(FIRST_OPTION);
+            flightsResultsPage.clickTicketVuelta(FIRST_OPTION + 1);
+
+            checkOutPageV3 = flightsResultsPage.clickComprarV3Btn(FIRST_OPTION);
+
+            dataManagement.getPassengerData("adult_male_native");
+            dataManagement.getPassengerData("adult_female_native");
+
+            checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                    "pago_dividido$deposit$1_master_master$",
+                    dataManagement.getBillingData("local_Billing_sucursales"),
+                    dataManagement.getContactData("contact_phone"),
+                    "FlightsCheckOutPageInternationalSucursal");
+
+            confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+            Assert.assertTrue(confirmationPageV3.confirmationOk());
+        }
+        else{
+            logger.warn("We are not running this for MEXICO!");
+        }
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void suc_Int_Booking_Flow_Splitted_TransferCredit() {
+        logTestTitle("Sucursales Flight Flow - International - Splitted 20 days - 2 Adults - Todas - " + countryPar);
+
+        if(!countryPar.equals("MEXICO")) {
+            dataManagement.getRoundTripDataTripItinerary("miami_10days_2adults_turista");
+
+            flightsDataTrip = basePage.flightsDataTrip();
+            flightsDataTrip.setOrigin(dataManagement.originAuto, dataManagement.originFull);
+            flightsDataTrip.setDestination(dataManagement.destinationAuto, dataManagement.destinationFull);
+            flightsDataTrip.selectDateFromCalendar(flightsDataTrip.departureFlightsCalendar, dataManagement.startDate);
+            flightsDataTrip.selectDateFromCalendar(flightsDataTrip.arrivalFlightsCalendar, dataManagement.endDate);
+            flightsDataTrip.selectPassenger(dataManagement.adults, dataManagement.childs);
+            flightsDataTrip.selectChildAgeRange(dataManagement.childAgeRange, dataManagement.childs);
+            flightsDataTrip.selectClass(dataManagement.flightClass);
+
+            flightsResultsPage = flightsDataTrip.clickBuscarBtn();
+
+            Assert.assertTrue(flightsResultsPage.vacancy());
+
+            flightsResultsPage.clickTicketIdaRdb(FIRST_OPTION);
+            flightsResultsPage.clickTicketVuelta(FIRST_OPTION + 1);
+
+            checkOutPageV3 = flightsResultsPage.clickComprarV3Btn(FIRST_OPTION);
+
+            dataManagement.getPassengerData("adult_male_native");
+            dataManagement.getPassengerData("adult_female_native");
+
+            checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                    "pago_dividido$transfer$1_visa_visa$",
+                    dataManagement.getBillingData("local_Billing_sucursales"),
+                    dataManagement.getContactData("contact_phone"),
+                    "FlightsCheckOutPageInternationalSucursal");
+
+            confirmationPageV3 = checkOutPageV3.clickComprarBtn();
+            Assert.assertTrue(confirmationPageV3.confirmationOk());
+        }
+        else{
+            logger.warn("We are not running this for MEXICO!");
+        }
+        setResultSauceLabs(PASSED);
+    }
+
+    @Test
+    public void suc_Int_Booking_Flow_Splitted_3cards() {
+        logTestTitle("Sucursales Flight Flow - International - 3 Cards - Splitted 20 days - 2 Adults - Todas - " + countryPar);
+
+        if(!countryPar.equals("MEXICO")) {
+            dataManagement.getRoundTripDataTripItinerary("miami_10days_2adults_turista");
+
+            flightsDataTrip = basePage.flightsDataTrip();
+            flightsDataTrip.setOrigin(dataManagement.originAuto, dataManagement.originFull);
+            flightsDataTrip.setDestination(dataManagement.destinationAuto, dataManagement.destinationFull);
+            flightsDataTrip.selectDateFromCalendar(flightsDataTrip.departureFlightsCalendar, dataManagement.startDate);
+            flightsDataTrip.selectDateFromCalendar(flightsDataTrip.arrivalFlightsCalendar, dataManagement.endDate);
+            flightsDataTrip.selectPassenger(dataManagement.adults, dataManagement.childs);
+            flightsDataTrip.selectChildAgeRange(dataManagement.childAgeRange, dataManagement.childs);
+            flightsDataTrip.selectClass(dataManagement.flightClass);
+
+            flightsResultsPage = flightsDataTrip.clickBuscarBtn();
+
+            Assert.assertTrue(flightsResultsPage.vacancy());
+
+            flightsResultsPage.clickTicketIdaRdb(FIRST_OPTION);
+            flightsResultsPage.clickTicketVuelta(FIRST_OPTION + 1);
+
+            checkOutPageV3 = flightsResultsPage.clickComprarV3Btn(FIRST_OPTION);
+
+            dataManagement.getPassengerData("adult_male_native");
+            dataManagement.getPassengerData("adult_female_native");
+
+            checkOutPageV3.populateCheckOutPageV3(dataManagement.passengerJsonList,
+                    "pago_dividido$1_amex_amex$1_visa_visa$1_master_master$",
                     dataManagement.getBillingData("local_Billing_sucursales"),
                     dataManagement.getContactData("contact_phone"),
                     "FlightsCheckOutPageInternationalSucursal");
