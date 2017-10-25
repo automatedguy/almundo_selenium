473936efbf94639e2a3371dd4cca26a732cd7cff
gabriel.cespedes <gabriel.cespedes@almundo.com>
2017-10-25 10:35:50 -0300

    CP-606: added for regression

---
+++ src/test/java/com/almundo/browser/automation/tests/Checkout/RetailFlights.java
@@ -0,0 +1,80 @@
+package com.almundo.browser.automation.tests.Checkout;
+
+import com.almundo.browser.automation.base.TestBaseSetup;
+import com.almundo.browser.automation.data.DataManagement;
+import com.almundo.browser.automation.pages.CheckOutPageV3.CheckOutPageV3;
+import com.almundo.browser.automation.pages.CheckOutPageV3.ThanksPageV3;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.almundo.browser.automation.utils.Constants.*;
+import static com.almundo.browser.automation.utils.Constants.Results.PASSED;
+
+/**
+ * Created by gabrielcespedes on 02/03/17.
+ */
+public class RetailFlights extends TestBaseSetup {
+
+    private DataManagement dataManagement = new DataManagement();
+    private CheckOutPageV3 checkOutPageV3 = null;
+    private ThanksPageV3 thanksPageV3 = null;
+    private final String productURl = "?product=flights&origin=flights";
+
+    @BeforeClass
+    private void initDataLists() {
+        retriesCount = true;
+        dataManagement.setPassengersList();
+        dataManagement.setPaymentList();
+        dataManagement.setBillingList();
+        dataManagement.setContactList();
+    }
+
+    @AfterMethod
+    private void cleanPassengerJsonList() {
+        dataManagement.clearPassengerJsonList();
+    }
+
+    private void getFlightsAssertionInfo(){
+        thanksPageAssertInfo.setFinalAmountPaid(checkOutPageV3.breakDownSectionV3().getFinalPriceString());
+        thanksPageAssertInfo.setFlightsDetailInfo(checkOutPageV3.breakDownSectionV3().getFlightDetailContent());
+        thanksPageAssertInfo.setContactEmailEntered(checkOutPageV3.contactSection().getContactEmail());
+    }
+
+    private void getPassengersData(){
+        logger.info("Getting Passenger Data.");
+        dataManagement.setPassengerData("adult_male_native");
+        dataManagement.setPassengerData("adult_female_native");
+        dataManagement.setPassengerData("child_male_native");
+    }
+    
+    /************************ Grid Test Area ************************/
+
+    @SuppressWarnings("Duplicates")
+    @Test
+    public void payWithLink() {
+        logTestTitle("Grid With Todo Pago" + countryPar );
+        checkOutPageV3 = openCart(cartId, "",productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.setCheckOutInfo(dataManagement.getPassengerJsonList(), LINK_VISA_1,
+                dataManagement.getBillingData(LOCAL_BILLING_SUCURSALES),
+                dataManagement.getContactData(CONTACT_PHONE), FLIGHTS_CHECKOUT_INT_RET);
+
+        getFlightsAssertionInfo();
+        thanksPageV3 = checkOutPageV3.clickComprarBtn();
+
+        Assert.assertTrue(thanksPageV3.confirmationOk());
+
+        //TODO: there is a bug related to final amount paid.
+        // Assert.assertTrue(thanksPageV3.isPaymentInfoOk(thanksPageAssertInfo.getFinalAmountPaid()));
+
+        Assert.assertTrue(thanksPageV3.isContactInfoOk(thanksPageAssertInfo.getContactEmailEntered()));
+        Assert.assertTrue(thanksPageV3.isFlightDetailInfoOk(thanksPageAssertInfo.getFlightDetailInfo()));
+        Assert.assertTrue(thanksPageV3.isPassengersInfoOk());
+
+        setResultSauceLabs(PASSED);
+    }
+}
