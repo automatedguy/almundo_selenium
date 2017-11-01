dc5925025c4ce1420ec426c0ab4a22ea073958e7
gabriel.cespedes <gabriel.cespedes@almundo.com>
2017-11-01 16:12:03 -0300

    CP-615: first commit

---
+++ src/test/java/com/almundo/browser/automation/tests/Checkout/RetailTrips.java
@@ -0,0 +1,63 @@
+package com.almundo.browser.automation.tests.Checkout;
+
+import com.almundo.browser.automation.base.TestBaseSetup;
+import com.almundo.browser.automation.data.DataManagement;
+import com.almundo.browser.automation.pages.CheckOutPageV3.CheckOutPageV3;
+import com.almundo.browser.automation.pages.CheckOutPageV3.ThanksPageV3;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static com.almundo.browser.automation.utils.Constants.*;
+import static com.almundo.browser.automation.utils.Constants.Results.PASSED;
+
+/**
+ * Created by gabrielcespedes on 02/03/17.
+ */
+public class RetailTrips extends TestBaseSetup {
+
+    private DataManagement dataManagement = new DataManagement();
+    private CheckOutPageV3 checkOutPageV3 = null;
+    private ThanksPageV3 thanksPageV3 = null;
+    private final String productURl = "?product=vuelohotel";
+
+    @BeforeClass
+    private void initDataLists() {
+        retriesCount = true;
+        dataManagement.setPassengersList();
+        dataManagement.setPaymentList();
+        dataManagement.setBillingList();
+        dataManagement.setContactList();
+    }
+
+    @AfterMethod
+    private void cleanPassengerJsonList() {
+        dataManagement.clearPassengerJsonList();
+    }
+
+    private void getPassengersData(){
+        logger.info("Getting Passenger Data.");
+        dataManagement.setPassengerData("adult_male_native");
+        dataManagement.setPassengerData("adult_female_native");
+        dataManagement.setPassengerData("child_male_native");
+    }
+    
+    /************************ Grid Test Area ************************/
+
+    @Test
+    public void payWithLink() {
+        logTestTitle("Trips Pay With Link");
+        checkOutPageV3 = openCart(cartId, "",productURl);
+
+        getPassengersData();
+
+        checkOutPageV3.setCheckOutInfo(dataManagement.getPassengerJsonList(),
+                                    LINK_VISA_1, dataManagement.getBillingData(LOCAL_BILLING),
+                                    dataManagement.getContactData(CONTACT_CELL_PHONE), TRIPS_CHECKOUT_DOM_RET);
+
+        thanksPageV3 = checkOutPageV3.clickComprarBtn();
+        Assert.assertTrue(thanksPageV3.confirmationOk());
+        setResultSauceLabs(PASSED);
+    }
+}
