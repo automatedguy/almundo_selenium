cb80ad815c0388e7808fea5e46c1871558ed437c
leandroefron <leandro.efron@almundo.com>
2018-02-05 16:36:26 -0300

    Update Airline locator

--- src/test/java/com/almundo/browser/automation/pages/CheckOutPageV3/CheckOutPageV3.java
+++ src/test/java/com/almundo/browser/automation/pages/CheckOutPageV3/CheckOutPageV3.java
@@ -143,7 +143,7 @@ public class CheckOutPageV3 extends TestBaseSetup {
     @FindBy(css = "#product-resume product-detail am-flights-cluster > div > div:nth-child(2) am-flight-choice .origin .iata.hint--top-right")
     public WebElement destinationAirport;
 
-    @FindBy(css = "#product-resume am-flights-cluster div:nth-child(1) > div > div > label am-flight-choice .airline > span")
+    @FindBy(css = ".flight-cluster > div:nth-child(1) .airline")
     public WebElement airlineName;
 
     @FindBy(css = ".flight-cluster > div:nth-child(1) .title > span.date")
