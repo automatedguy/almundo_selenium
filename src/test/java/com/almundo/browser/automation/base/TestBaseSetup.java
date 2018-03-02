e2ce99a1568cbce849b25f31c9a595c260e71d02
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-03-02 18:58:06 -0300

    added the ab router thing

--- src/test/java/com/almundo/browser/automation/base/TestBaseSetup.java
+++ src/test/java/com/almundo/browser/automation/base/TestBaseSetup.java
@@ -104,9 +104,9 @@ public class TestBaseSetup {
                                         @Optional(LATEST) String browserTypeVersion,
                                         @Optional(ARGENTINA) String country,
                                         @Optional(TRUE) Boolean landing,
-                                        @Optional("") String cart_id,
+                                        @Optional("5a99c43124aa9a000b9888dd") String cart_id,
                                         @Optional("") String cart_id_icbc,
-                                        @Optional(FALSE) Boolean submit_Reservation,
+                                        @Optional(TRUE) Boolean submit_Reservation,
                                         @Optional("") String provider_header,
                                         @Optional(FALSE) Boolean florida_header,
                                         @Optional(FALSE) Boolean retries_Max_Count) {
