dd3a8f6eb83165e07a5033c936a8ab0abe93bceb
gabriel.cespedes <gabriel.cespedes@almundo.com>
2018-03-09 09:29:31 -0300

    Fixed 2 cards insurance

--- src/test/java/com/almundo/browser/automation/pages/CheckOutPageV3/CheckOutPageV3.java
+++ src/test/java/com/almundo/browser/automation/pages/CheckOutPageV3/CheckOutPageV3.java
@@ -403,9 +403,9 @@ public class CheckOutPageV3 extends TestBaseSetup {
     }
 
     private void dealWithTwoCards(String paymentData){
+        breakDownSectionV3().dealWithInsurance(addInsurance);
         paymentSelectorV3().selectTwoCreditCardsRdb();
         paymentTwoCreditCardsV3().populateTwoCreditCards(getPaymentDataList(paymentData.replace("two_cards$","")), breakDownSectionV3().getFinalPrice());
-        // breakDownSectionV3().dealWithInsurance(addInsurance);
         if(refillPaymentData){
             paymentTwoCreditCardsV3().populateTwoCreditCards(getPaymentDataList(paymentData.replace("two_cards$","")), breakDownSectionV3().getFinalPrice());
         }
